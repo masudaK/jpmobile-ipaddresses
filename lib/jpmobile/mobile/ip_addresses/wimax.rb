5ac7e90bf270c2e217ac67c2d4772e10e4ef30bb
masudaK <masuken@gmail.com>
2014-05-10 13:28:37 +0900

    wimaxの設定追加

---
+++ lib/jpmobile/mobile/ip_addresses/wimax.rb
@@ -0,0 +1,16 @@
+module Jpmobile
+  module Mobile
+    module IpAddresses
+      class Emobile < AbstractIpAddresses
+        class << self
+          def ip_address_list
+            @@ip_address_list ||= [
+              "114.22.0.0/16",
+              "113.152.64.0/18"
+            ].map {|ip| IPAddr.new(ip) }
+          end
+        end
+      end
+    end
+  end
+end
